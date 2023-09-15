42299928959cb1d4f88d337b8e170dd6d9e05d77
Madeliene <madelienestiller@gmail.com>
2023-09-15 13:13:53 +1000

    modifying DOM lab complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+
+main.remove();
+
+
+
+
+const newHeader = document.createElement("h1");
+
+const textNode = document.createTextNode("Madeliene is the champion");
+
+newHeader.appendChild(textNode);
+
+newHeader.setAttribute('id', 'victory');
+
